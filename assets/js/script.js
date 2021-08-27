c512a9d52aa3cfb1595f5c55196409e6b486aeca
corneezius <cfr2116@columbia.edu>
2021-08-26 20:37:11 -0400

    udpate loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 		$('#rumble-credit').fadeIn(550);
     //   // $('navbar').hide(0);
 		//
-		setTimeout(function(){
-			$('#rumble-credit').hide();
-			// $('.borboletas').fadeOut(1500);
-		}, 1250);
+		// setTimeout(function(){
+		// 	$('#rumble-credit').hide();
+		// 	// $('.borboletas').fadeOut(1500);
+		// }, 1250);
 
 		// setTimeout(function(){
 		// 	$('#project-title').show();
@@ -19,11 +19,11 @@ $(document).ready(function() {
 		// 	$('#bars').hide();
 		// 	$('#artist-name').hide();
 		//
-		setTimeout(function(){
-			$('body').removeClass("noscroll");
-			$('body').addClass('loaded');
-      $('#mainNav').removeClass('none');
-		}, 1750);
+		// setTimeout(function(){
+		// 	$('body').removeClass("noscroll");
+		// 	$('body').addClass('loaded');
+    //   $('#mainNav').removeClass('none');
+		// }, 1750);
 
 
 	//   setTimeout(function(){
